e840a625551062354135766516a31ac6a1191113
Vivek Singh <petmongrels@gmail.com>
2018-01-24 13:56:14 +0530

    reusable join added

--- utilities/index.js
+++ utilities/index.js
@@ -2,11 +2,22 @@
 // IT IS NOT MANDATORY THAT ALL THE SQL ARE GENERATED THIS FASHION. USE IT FOR ELIMINATING DUPLICATION OF CODE AND SCALE OF DUPLICATION
 let mustache = require('mustache');
 
+let programEncounterJoin = `FROM program_encounter
+INNER JOIN program_enrolment ON program_encounter.program_enrolment_id = program_enrolment.id
+INNER JOIN encounter_type ON program_encounter.encounter_type_id = encounter_type.id
+INNER JOIN program ON program_enrolment.program_id = program.id
+WHERE program.name = 'Adolescent'`;
+
 let serviceDelivery = {
     indicators: [
         {
-            indicator: 'Total Adolescents Enrolled',
+            indicator: 'Total adolescents Enrolled',
             from: 'adolescents',
+            whereClause: ''
+        },
+        {
+            indicator: 'Total adolescents having problem',
+            from: 'adolescent_visit_summary',
             whereClause: ''
         }
     ]
@@ -26,3 +37,6 @@ let template = `{{#indicators}}SELECT '{{indicator}}',
 {{/indicators}}`;
 
 console.log(mustache.render(template, serviceDelivery));
+
+// coded_obs_exists(program_encounter, 'Refer to hospital for') referred,
+// encounter_type.name = 'Dropout Home Visit' AS home_visit_done

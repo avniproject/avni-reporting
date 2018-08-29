aa9b732868859f8c3f04907d3ef3085f41b682fd
mihirk <mihir.khatwani@gmail.com>
2018-08-29 10:25:12 +0530

    Adding albendazole tablets received report

--- utilities/generatedSQL/Iron Tables.sql
+++ utilities/generatedSQL/Iron Tables.sql
@@ -88,7 +88,49 @@ FROM all_program_entire_enrolment lpe
   LEFT OUTER JOIN individual i ON i.uuid = lpe.iuuid
   LEFT OUTER JOIN address_level a ON i.address_id = a.id
   LEFT OUTER JOIN gender g ON i.gender_id = g.id
-WHERE lpe.obs ->> ''''2973890d-0142-41a6-a6f5-988e7eb36d79'''' = ''''04bb1773-c353-44a1-a68c-9b448e07ff70'''''')') AS (
+WHERE lpe.obs ->> ''''2973890d-0142-41a6-a6f5-988e7eb36d79'''' = ''''04bb1773-c353-44a1-a68c-9b448e07ff70'''''')
+UNION ALL
+SELECT
+''Albandazole Tablets Received''                                          rowid,
+address_type || '' '' || gender AS                             attribute,
+total :: VARCHAR || '' ('' || percentage :: VARCHAR(5) || ''%)'' frequency_percentage
+FROM frequency_and_percentage(''WITH all_program_entire_enrolment AS (
+    SELECT
+      i.uuid AS                                                  iuuid,
+      jsonb_merge(jsonb_agg(jsonb_strip_nulls(pe.observations))) obs
+    FROM program_encounter pe
+      INNER JOIN program_enrolment e ON pe.program_enrolment_id = e.id
+      INNER JOIN individual i ON e.individual_id = i.id
+      INNER JOIN encounter_type et ON pe.encounter_type_id = et.id
+      INNER JOIN program p ON p.id = e.program_id
+    WHERE p.name = ''''Adolescent''''
+          AND pe.encounter_date_time IS NOT NULL
+    GROUP BY i.uuid
+)
+SELECT
+  lpe.iuuid uuid,
+  g.name    gender_name,
+  a.type    address_type,
+  a.title   address_name
+FROM all_program_entire_enrolment lpe
+  LEFT OUTER JOIN individual i ON i.uuid = lpe.iuuid
+  LEFT OUTER JOIN address_level a ON i.address_id = a.id
+  LEFT OUTER JOIN gender g ON i.gender_id = g.id
+WHERE lpe.obs ->> ''''a2e181c3-0827-4da2-9121-a59386449823'''' = ''''04bb1773-c353-44a1-a68c-9b448e07ff70'''''', ''SELECT
+  DISTINCT
+  i.uuid  uuid,
+  g.name  gender_name,
+  a.type  address_type,
+  a.title address_name
+FROM
+  program_encounter pe
+  LEFT OUTER JOIN encounter_type et ON et.id = pe.encounter_type_id
+  LEFT OUTER JOIN program_enrolment enrolment ON pe.program_enrolment_id = enrolment.id
+  LEFT OUTER JOIN program p ON enrolment.program_id = p.id
+  LEFT OUTER JOIN individual i ON enrolment.individual_id = i.id
+  LEFT OUTER JOIN gender g ON g.id = i.gender_id
+  LEFT OUTER JOIN address_level a ON i.address_id = a.id
+WHERE p.name = ''''Adolescent'''' AND et.name = ''''Annual Visit'''''')') AS (
 rowid TEXT,
 "All Female" TEXT,
 "All Male" TEXT,

295f0c89dc34b345ad556ba6987ed98f176dd9ad
Sidtharthan A N <sidthunagarajan@gmail.com>
2018-12-20 17:38:09 +0530

    1040 | Format utilities/views/views.sql

--- utilities/views/views.sql
+++ utilities/views/views.sql
@@ -151,8 +151,10 @@ create view all_enrolment_unplanned_encounters_agg_view AS
   from agg
          join operational_program_view op on op.program_id = agg.program_id;
 
-SELECT grant_all_on_all(a.rolname) FROM pg_roles a WHERE pg_has_role('openchs', a.oid, 'member')
-                                                     and a.rolsuper is false
-                                                     and a.rolname not like 'pg%'
-                                                     and a.rolname not like 'rds%'
+SELECT grant_all_on_all(a.rolname)
+FROM pg_roles a
+WHERE pg_has_role('openchs', a.oid, 'member')
+  and a.rolsuper is false
+  and a.rolname not like 'pg%'
+  and a.rolname not like 'rds%'
 order by a.rolname;

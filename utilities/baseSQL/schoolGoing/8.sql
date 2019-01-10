87a7b3e46056dfb2721293061cfc80be64aeb685
rsatishm <rsatish.m@gmail.com>
2019-01-10 10:43:56 +0530

    fixed column name to agg_obs

--- utilities/baseSQL/schoolGoing/8.sql
+++ utilities/baseSQL/schoolGoing/8.sql
@@ -5,4 +5,4 @@ SELECT i.uuid as uuid,
 FROM non_exited_enrolment_completed_encounters_agg_view lpe
       JOIN individual_gender_address_view i ON i.id = lpe.individual_id
 WHERE lpe.program_name = 'Adolescent'
-      AND lpe.obs @> '{"fda18675-59dc-4a69-82f7-4e137c274073":"d7443a73-80b7-4320-9c45-6558595be70a"}'
+      AND lpe.agg_obs @> '{"fda18675-59dc-4a69-82f7-4e137c274073":"d7443a73-80b7-4320-9c45-6558595be70a"}'
--- utilities/generatedSQL/Standard8And9And10.sql
+++ utilities/generatedSQL/Standard8And9And10.sql
@@ -9,7 +9,7 @@ FROM frequency_and_percentage(''SELECT i.uuid as uuid,
 FROM non_exited_enrolment_completed_encounters_agg_view lpe
       JOIN individual_gender_address_view i ON i.id = lpe.individual_id
 WHERE lpe.program_name = ''''Adolescent''''
-      AND lpe.obs @> ''''{"fda18675-59dc-4a69-82f7-4e137c274073":"d7443a73-80b7-4320-9c45-6558595be70a"}''''
+      AND lpe.agg_obs @> ''''{"fda18675-59dc-4a69-82f7-4e137c274073":"d7443a73-80b7-4320-9c45-6558595be70a"}''''
 '', ''SELECT i.uuid as uuid,
        i.gender as gender_name,
        i.addresslevel_type as address_type,

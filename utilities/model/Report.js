74820272e6db9b902ee6e6e514088eba18cb8d6a
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-04-15 17:56:14 +0530

    0000 | Add Report name to the generated sql for easy back track

--- utilities/model/Report.js
+++ utilities/model/Report.js
@@ -12,6 +12,7 @@ class Report {
         const pivotTemplate = fs.readFileSync(["templates", "pivotToGenderAndAddress.mustache"].join("/"), "utf-8");
         let allSubQueries = `${this.subReports.map((subReport) => subReport.getWrappedSQL(this.dirName)).join("\nUNION ALL\n")}`;
         return Mustache.render(pivotTemplate, {
+            fileName: this.name,
             allSubQueries: allSubQueries.replace(/'/g, "''")
         });
     }

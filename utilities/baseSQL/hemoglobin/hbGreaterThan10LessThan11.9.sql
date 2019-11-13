0af1b33b65de5cda3ea56170bfe2f97e5d211872
Vinod Deolal <vinoddeolal@gmail.com>
2019-11-13 12:59:23 +0530

    #48 | Bug in Prevalence of Anemia Report

--- utilities/baseSQL/hemoglobin/hbGreaterThan10LessThan11.9.sql
+++ utilities/baseSQL/hemoglobin/hbGreaterThan10LessThan11.9.sql
@@ -10,6 +10,7 @@ WITH individual_program_partitions AS (
          INNER JOIN address_level a ON a.id = i.address_id
   WHERE e.program_name = 'Adolescent'
     AND (pe.encounter_type_name = 'Annual Visit' or pe.encounter_type_name = 'Quarterly Visit')
+    AND pe.observations ->> 'f9ecabbc-2df2-4bfc-a6fa-aa417c50e11b' notnull
     [[ and e.enrolment_date_time >=(q1 || q4 || quote_literal({{ start_date }}) || q4 || q1  ::DATE)]]
     [[and e.enrolment_date_time <=q1 || q4 || quote_literal({{end_date}}) || q4 || q1 ::DATE]]
     [[and a.title = q1 || q4 || quote_literal({{title}}) || q4 || q1]]

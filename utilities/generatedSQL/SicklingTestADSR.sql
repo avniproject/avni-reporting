79eb25867f2b6d1c5aab0c09f25b8a6dd6e8ce5d
Gojo-Taqi <modtaqi143@gmail.com>
2022-11-11 12:29:32 +0530

    #191 | Added two extra indicators for the report prevalence of sickle cell anemia

--- utilities/generatedSQL/SicklingTestADSR.sql
+++ utilities/generatedSQL/SicklingTestADSR.sql
@@ -62,6 +62,122 @@ WHERE program_exit_date_time is null  AND i.is_voided = false and a.is_voided = 
 [[and a.title  = ' || '''' || quote_literal({{title}}) || '''' || ']]'')
 UNION ALL
 SELECT
+''Sickle cell disease''                                          rowid,
+address_type || '' '' || gender AS                             attribute,
+total :: VARCHAR || '' ('' || percentage :: VARCHAR(5) || ''%)'' frequency_percentage
+FROM frequency_and_percentage(''with agg_data as (select uuid,
+                         individual_id,
+                         program_enrolment_id,
+                         encounter_date_time,
+                         "Sickling test result"
+from adsr.individual_adolescent_annual_visit_baseline
+where encounter_date_time is not null
+union all
+select uuid,
+       individual_id,
+       program_enrolment_id,
+       encounter_date_time,
+       "Sickling test result"
+from adsr.individual_adolescent_annual_visit_endline
+where encounter_date_time is not null),
+
+  individual_program_partitions AS (SELECT i.uuid          AS                                                           iuuid,
+                                              row_number() OVER (PARTITION BY i.uuid ORDER BY pe.encounter_date_time desc) erank,
+                                              pe.uuid         AS                                                           euuid,
+pe."Sickling test result" AS                                                           obs,
+pe.encounter_date_time
+FROM agg_data pe
+INNER JOIN adsr.individual_adolescent e ON pe.program_enrolment_id = e.id
+INNER JOIN adsr.individual i ON e.individual_id = i.id
+INNER JOIN address_level a ON a.id = i.address_id
+WHERE e.program_exit_date_time is null
+AND pe."Sickling test result" = ''''Disease''''
+[[and e.enrolment_date_time >=(' || '''' || quote_literal({{ start_date }}) || '''' || '  ::DATE)]]
+[[and e.enrolment_date_time <=' || '''' || quote_literal({{end_date}}) || '''' || ' ::DATE]]
+[[and a.title = ' || '''' || quote_literal({{title}}) || '''' || ']]
+--
+)
+
+select distinct i.uuid              as uuid,
+                i.gender            as gender_name,
+                alt.name as address_type,
+                a.title as address_name
+FROM individual_program_partitions ip
+JOIN adsr.individual i ON i.uuid = ip.iuuid
+JOIN address_level a on a.id = i.address_id
+JOIN address_level_type alt on alt.id = a.type_id
+WHERE ip.erank = 1'', ''SELECT DISTINCT i.uuid uuid, i.gender gender_name, alt.name address_type, a.title address_name
+FROM program_enrolment enrolment
+LEFT OUTER JOIN adsr.individual i
+ON enrolment.individual_id = i.id
+JOIN address_level a
+ON a.id = i.address_id
+JOIN address_level_type alt
+ON alt.id = a.type_id
+WHERE program_exit_date_time is null  AND i.is_voided = false and a.is_voided = false
+[[ and enrolment.enrolment_date_time >=(' || '''' || quote_literal({{ start_date }}) || '''' || '  ::DATE)]]
+[[and enrolment.enrolment_date_time <=' || '''' || quote_literal({{end_date}}) || '''' || ' ::DATE]]
+[[and a.title  = ' || '''' || quote_literal({{title}}) || '''' || ']]'')
+UNION ALL
+SELECT
+''Sickle cell trait''                                          rowid,
+address_type || '' '' || gender AS                             attribute,
+total :: VARCHAR || '' ('' || percentage :: VARCHAR(5) || ''%)'' frequency_percentage
+FROM frequency_and_percentage(''with agg_data as (select uuid,
+                         individual_id,
+                         program_enrolment_id,
+                         encounter_date_time,
+                         "Sickling test result"
+from adsr.individual_adolescent_annual_visit_baseline
+where encounter_date_time is not null
+union all
+select uuid,
+       individual_id,
+       program_enrolment_id,
+       encounter_date_time,
+       "Sickling test result"
+from adsr.individual_adolescent_annual_visit_endline
+where encounter_date_time is not null),
+
+  individual_program_partitions AS (SELECT i.uuid          AS                                                           iuuid,
+                                              row_number() OVER (PARTITION BY i.uuid ORDER BY pe.encounter_date_time desc) erank,
+                                              pe.uuid         AS                                                           euuid,
+pe."Sickling test result" AS                                                           obs,
+pe.encounter_date_time
+FROM agg_data pe
+INNER JOIN adsr.individual_adolescent e ON pe.program_enrolment_id = e.id
+INNER JOIN adsr.individual i ON e.individual_id = i.id
+INNER JOIN address_level a ON a.id = i.address_id
+WHERE e.program_exit_date_time is null
+AND pe."Sickling test result" = ''''Trait''''
+[[and e.enrolment_date_time >=(' || '''' || quote_literal({{ start_date }}) || '''' || '  ::DATE)]]
+[[and e.enrolment_date_time <=' || '''' || quote_literal({{end_date}}) || '''' || ' ::DATE]]
+[[and a.title = ' || '''' || quote_literal({{title}}) || '''' || ']]
+--
+)
+
+select distinct i.uuid              as uuid,
+                i.gender            as gender_name,
+                alt.name as address_type,
+                a.title as address_name
+FROM individual_program_partitions ip
+JOIN adsr.individual i ON i.uuid = ip.iuuid
+JOIN address_level a on a.id = i.address_id
+JOIN address_level_type alt on alt.id = a.type_id
+WHERE ip.erank = 1'', ''SELECT DISTINCT i.uuid uuid, i.gender gender_name, alt.name address_type, a.title address_name
+FROM program_enrolment enrolment
+LEFT OUTER JOIN adsr.individual i
+ON enrolment.individual_id = i.id
+JOIN address_level a
+ON a.id = i.address_id
+JOIN address_level_type alt
+ON alt.id = a.type_id
+WHERE program_exit_date_time is null  AND i.is_voided = false and a.is_voided = false
+[[ and enrolment.enrolment_date_time >=(' || '''' || quote_literal({{ start_date }}) || '''' || '  ::DATE)]]
+[[and enrolment.enrolment_date_time <=' || '''' || quote_literal({{end_date}}) || '''' || ' ::DATE]]
+[[and a.title  = ' || '''' || quote_literal({{title}}) || '''' || ']]'')
+UNION ALL
+SELECT
 ''Negative''                                          rowid,
 address_type || '' '' || gender AS                             attribute,
 total :: VARCHAR || '' ('' || percentage :: VARCHAR(5) || ''%)'' frequency_percentage
